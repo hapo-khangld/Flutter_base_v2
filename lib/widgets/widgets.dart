3520daa0b709c4d88e8b58d8b7b598fc8ddbb577
Nhat Nam Hoang <nam.hoang@the-hash.com>
2022-02-17 15:39:02 +0700

    fix : remove unuse widgets

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,4 +3,4 @@ export 'custom_smart_refresh/custom_smart_refresh.dart';
 export 'image_widget/image_widget.dart';
 export 'empty_view/empty_view.dart';
 export 'error_view/error_view.dart';
-export 'pop_up_alert/pop_up_alert.dart';
+export 'custom_alert_dialog/custom_alert_dialog.dart';
